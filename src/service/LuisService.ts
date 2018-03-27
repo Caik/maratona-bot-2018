bac73615ef18400d40424fff195ebd07e95e3826
Caik <carlos@amlconsulting.com.br>
2018-03-27 15:41:23 -0300

    Salvo

--- src/service/LuisService.ts
+++ src/service/LuisService.ts
@@ -2,7 +2,7 @@ import { Luis } from "../bot/Luis";
 
 export class LuisService {
 
-    public async processTerm(term: string) {
+    public static async processTerm(term: string) {
         const luis = new Luis();
 	    const result = await luis.getResult(term);
     }
