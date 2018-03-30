8bc080e8ee535164c78f059f7763abcad0afe3fd
Carlos Henrique <caiik@live.com>
2018-03-30 00:59:23 -0300

    Ajustes no deploy

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,11 +1,16 @@
 import * as mongoose from "mongoose";
 
-mongoose.connect(process.env.COSMOSDB_HOST, {
-	auth: {
-		user: process.env.COSMOSDB_USER,
-		password: process.env.COSMOSDB_PASSWORD
-	}
-});
+mongoose
+	.connect(process.env.COSMOSDB_HOST, {
+		auth: {
+			user: process.env.COSMOSDB_USER,
+			password: process.env.COSMOSDB_PASSWORD
+		}
+	})
+	.catch(error => {
+		console.error(error);
+		console.log("Error on connecto to Cosmos DB");
+	});
 
 export const db = mongoose.connection;
 db.once("open", () => console.log("Connected to Cosmos DB"));
